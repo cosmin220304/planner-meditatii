8160adb4b80fe1955095f19720c19b12c219b8ad
Pricope Marius-Andrei <enjoycoding10@gmail.com>
2023-03-25 17:55:51 +0200

    remove nextprev button, it wasuseless

--- src/components/NextPrevButton/NextPrevButton.tsx
+++
@@ -1,8 +0,0 @@
-import react from "react"
-
-export default function NextPrevButton(props : any)
-{
-    return (
-        <button className="h-fit w-fit bg-sky-400 p-12"><img src=""></img></button>
-    )
-}

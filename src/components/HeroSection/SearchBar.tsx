d353634090e8a7d2147faf8e7ed9452f2fc98846
Pricope Marius-Andrei <enjoycoding10@gmail.com>
2023-03-25 17:52:36 +0200

    add a new carousel

--- src/components/HeroSection/SearchBar.tsx
+++ src/components/HeroSection/SearchBar.tsx
@@ -5,7 +5,7 @@ function SearchBar() {
     return (
         <section className="drop-shadow-lg rounded-xl p-0 flex bg-white max-w-fit">
             <FaSearch className="m-auto mr-5 ml-5 h-5 w-5 text-sky-500"/>
-            <input className="w-96 min-w-max p-3.5 rounded-l-xl border-0 focus:outline-none" placeholder="Programming lessons! John..."></input>
+            <input className="md:w-96 md:min-w-max w-full p-3.5 rounded-l-xl border-0 focus:outline-none" placeholder="Programming lessons! John..."></input>
             <button className="m-0 p-4 bg-sky-500 rounded-r-xl text-slate-50 font-bold" type="submit">Search</button>
         </section>
     )
---
+++ src/components/NextPrevButton/NextPrevButton.tsx
@@ -0,0 +1,8 @@
+import react from "react"
+
+export default function NextPrevButton(props : any)
+{
+    return (
+        <button className="h-fit w-fit bg-sky-400 p-12"><img src=""></img></button>
+    )
+}

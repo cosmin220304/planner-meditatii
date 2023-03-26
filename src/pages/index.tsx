d5fdad81e293f11069adf318f7921fb79cabefbc
cosmin220304 <cosmin.aftanase@yahoo.com>
2023-03-26 17:49:19 +0300

    added Description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import Topnav from "@features/topnav";
+import Description from "@components/Description";
 
 function Home() {
   return (
     <div>
       <Topnav />
-      Home page
+      <Description />
     </div>
   );
 }
